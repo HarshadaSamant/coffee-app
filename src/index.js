9533cea682e3313adb8e31ed70f6a8db4ebeb4bb
SamantHarshada <>
2019-03-19 12:48:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './stylesheets/style.css';
 import * as serviceWorker from './serviceWorker';
+import { render } from "react-dom";
+import {BrowserRouter, Route, Link} from "react-router-dom";
+
+// const Home = () => <div> Home </div>;
+// const About = () => <div> About </div>;
+// const Contact = ({match}) => <div>{match.url}</div>;
+
+const App = () => (
+    <BrowserRouter>
+        <section className="first">
+            <form className="signupForm">
+                <header>
+                    <h2>Get Started</h2>
+                    <hr/>
+                    <p>first step towards innovation</p>
+                </header>
+                    <div className="wrapper">   
+                        <div className="content">
+                            <h2>Sign Up</h2>
+                            <div className="input_field">
+                                <label>User Name:</label>
+                                <input type="text" name="uname" placeholder="Enter User Name"></input>
+                            </div>
+                            <div className="input_field">
+                                <label>User Id:</label>
+                                <input type="email" name="email" placeholder="Enter User Id"></input>
+                            </div>
+                            <div className="input_field">
+                                <label>Password:</label>
+                                <input type="password" name="pass" placeholder="Enter Password"></input>
+                            </div>
+                            <div className="input_field">
+                                <label>Confirm Password:</label>
+                                <input type="password" name="pass" placeholder="Enter Password"></input>
+                            </div>
+                        </div> 
+                    </div>
+            </form>
+        </section>
+    </BrowserRouter>
+);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
